f78d92bd3d4be1af8e1940c6609d657fdeef2e2f
Aritra Biswas <aritrabb@gmail.com>
2026-05-16 00:12:52 +0200

    Add CLI using clap.
    Signed-off-by: Aritra Biswas <aritrabb@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,27 @@
-use image::{DynamicImage, GenericImageView};
-use image::imageops::{FilterType};
-use std::path::{Path, PathBuf};
-use std::fs;
+mod pixelize;
 
-const DEFAULT_FILTER: FilterType = FilterType::Nearest;
+use pixelize::pixelize;
+use clap::Parser;
 
-// NOTE: Downscale height can theoretically be any u32 but keep it 64, 128, 256
-fn initial_downscale(img: &DynamicImage, downscale_height: u32) -> DynamicImage {
-    let (original_width, original_height) = img.dimensions();
-    let downscale_width = (original_width * downscale_height) / original_height;
+/// Simple conversion tool to convert regular JPG images to pixel art version of it
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to the JPG image
+    #[arg(short, long)]
+    imagepath: String,
 
-    img.resize(downscale_width, downscale_height, DEFAULT_FILTER)
-}
+    /// Downscaling height value in pixels
+    #[arg(short, long, default_value_t = 128)]
+    downscale: u8,
 
-// Ideally make new_width and new_height as the original JPG dimensions
-// Debate over using resize vs resize_exact
-fn upscale(downscaled_img: &DynamicImage, new_width: u32, new_height: u32) -> DynamicImage{
-    downscaled_img.resize(new_width, new_height, DEFAULT_FILTER)
-}
-
-fn pixelize(image_path: &str, downscale_height: u32) -> Result<(), Box<dyn std::error::Error>>{
-    let img_path = Path::new(image_path);
-    let img = image::open(img_path)?;
-    let (img_width, img_height) = img.dimensions();
-    println!("Original image dimensions {} x {}", img_width, img_height);
-
-    let dir = PathBuf::from(
-        img_path
-            .file_stem()
-            .and_then(|f| {f.to_str()})
-            .unwrap_or("output")
-    );
-
-    fs::create_dir_all(&dir)?;
-
-    let new_img = initial_downscale(&img, downscale_height);
-    let (new_width, new_height) = new_img.dimensions();
-    println!("Downscaled image dimensions {} x {}", new_width, new_height);
-    new_img.save(dir.join(format!("downscaled_{downscale_height}.jpg")))?;
-
-    let final_upscale = upscale(&new_img, img_width, img_height);
-    final_upscale.save(dir.join(format!("final_{downscale_height}.jpg")))?;
-
-    println!("======== DONE ===========");
-    println!("Final image upscale resolution --- {} x {}", img_width, img_height);
-
-    Ok(())
+    /// Includes verbose output of image processing
+    #[arg(short, long, default_value_t = false)]
+    verbose: bool,
 }
 
 fn main(){
-    pixelize("bluemarble.jpg", 128).expect("Something went horribly wrong");
+    let args = Args::parse();
+
+    pixelize(args.imagepath.as_str(), args.downscale as u32, args.verbose).expect("Something went horribly wrong");
 }
---
+++ src/pixelize.rs
@@ -0,0 +1,59 @@
+use image::{DynamicImage, GenericImageView};
+use image::imageops::{FilterType};
+use std::path::{Path, PathBuf};
+use std::fs;
+
+const DEFAULT_FILTER: FilterType = FilterType::Nearest;
+
+// NOTE: Downscale height can theoretically be any u32 but keep it 64, 128, 256
+fn initial_downscale(img: &DynamicImage, downscale_height: u32) -> DynamicImage {
+    let (original_width, original_height) = img.dimensions();
+    let downscale_width = (original_width * downscale_height) / original_height;
+
+    img.resize(downscale_width, downscale_height, DEFAULT_FILTER)
+}
+
+// Ideally make new_width and new_height as the original JPG dimensions
+// Debate over using resize vs resize_exact
+fn upscale(downscaled_img: &DynamicImage, new_width: u32, new_height: u32) -> DynamicImage{
+    downscaled_img.resize(new_width, new_height, DEFAULT_FILTER)
+}
+
+pub fn pixelize(image_path: &str, downscale_height: u32, verbose: bool) -> Result<(), Box<dyn std::error::Error>>{
+    let img_path = Path::new(image_path);
+    let img = image::open(img_path)?;
+    let (img_width, img_height) = img.dimensions();
+
+    println!("Input Image: {image_path} ({img_width}x{img_height})");
+
+    let dir = PathBuf::from(
+        img_path
+            .file_stem()
+            .and_then(|f| {f.to_str()})
+            .unwrap_or("output")
+    );
+
+    fs::create_dir_all(&dir)?;
+
+    let new_img = initial_downscale(&img, downscale_height);
+    let (new_width, new_height) = new_img.dimensions();
+    if verbose {
+        println!("[LOG] Downscaled image dimensions {new_width} x {new_height}");
+    }
+    let ds_format_path = dir.join(format!("downscaled_{downscale_height}.jpg"));
+    new_img.save(&ds_format_path)?;
+
+    let final_upscale = upscale(&new_img, img_width, img_height);
+    let (final_width, final_height) = new_img.dimensions();
+    let upscale_format_path = dir.join(format!("final_{downscale_height}.jpg"));
+    final_upscale.save(&upscale_format_path)?;
+
+    println!("======== Processing Complete ===========");
+    if verbose {
+        println!("[LOG] Final image upscale resolution --- {} x {}", final_width, final_height);
+    }
+    println!("Downscaled image saved at {:?}", ds_format_path);
+    println!("Final output (upscaled) saved at {:?}", upscale_format_path);
+
+    Ok(())
+}
